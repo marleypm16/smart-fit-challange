d134187e9a2c5e40bfddbbf9dc6a9119f748f229
marleypm16 <yarlempm16@gmail.com>
2024-03-01 20:59:14 -0300

    home changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
